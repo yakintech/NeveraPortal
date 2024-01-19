6ded15740621f6103bb2d98843085e150f585e2e
Selim Şahin <selimsahin.sns@gmail.com>
2024-01-19 20:48:06 +0300

    created blog index page, basic blog controller, view models

---
+++ NeveraPortal.UI/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NeveraPortal.BLL.Services.Repositories;
+using NeveraPortal.DAL.Models;
+using NeveraPortal.UI.Areas.Admin.Models.AdminUser;
+
+namespace NeveraPortal.UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        private GenericRepository<Blog> _blogRepository;
+
+        public BlogController()
+        {
+            _blogRepository = new GenericRepository<Blog>();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create(CreateBlogVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                Blog blog = new Blog
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    MainImgPath = model.MainImgPath,
+                    Tags = model.Tags
+                };
+
+                _blogRepository.Create(blog);
+                return View();
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(EditAdminUserVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ NeveraPortal.UI/Areas/Admin/Models/Blog/CreateBlogVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeveraPortal.UI.Areas.Admin.Models.AdminUser
+{
+    public class CreateBlogVM
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        public string MainImgPath { get; set; } = string.Empty;
+
+        public string Tags { get; set; } = string.Empty;
+    }
+}
